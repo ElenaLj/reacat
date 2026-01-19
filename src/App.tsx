bc3af810a1242b2b22701cdc0566f874822478b2
Elena Ljubijankic <elenaljubijankic@gmail.com>
2026-01-19 12:42:54 +0100

    add random assets

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,31 @@ const cats: Cat = [
   {
     id: 0,
     name: "Merlino",
+    gender: "M",
     age: 2,
     colour: "white and black",
-    desc: "lorem ipsum Merlino"
+    desc: "lorem ipsum Merlino",
+    image: "https://cdn2.thecatapi.com/images/4os.jpg"
   },
   {
     id: 1,
     name: "Milly",
+    gender: "F",
     age: 2,
     colour: "black and white",
-    desc: "lorem ipsum Milly"
+    desc: "lorem ipsum Milly",
+    image: "https://cdn2.thecatapi.com/images/7cn.jpg"
   },
 ]
 
 type Cat = {
   id: number,
   name: string,
+  gender: "M" | "F";
   age: number,
   colour: string,
-  desc: string
+  desc: string,
+  image: string
 }[]
 
 // const catName = cats.map((cat) => <p>{cat.name}</p>);
@@ -35,7 +41,6 @@ function App() {
 
   return (
     <>
-
       <Card></Card>
     </>
   )
@@ -44,10 +49,19 @@ function App() {
 function Card() {
   const catsList = cats.map(cat =>
     <li>
-      <p>{cat.name}</p>
-      <p>{cat.age}</p>
-      <p>{cat.desc}</p>
-    </li>)
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">{cat.name}, {cat.age}</h2>
+          <p>{cat.desc}</p>
+        </div>
+        <figure>
+          <img
+            src={cat.image}
+            alt={cat.name} />
+        </figure>
+      </div>
+    </li>
+  )
   return catsList
 }
 
