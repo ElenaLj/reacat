947018d6f39baa80bdc4e3ee9676e6acf90e7cc8
Elena Ljubijankic <elenaljubijankic@gmail.com>
2026-01-22 18:00:36 +0100

    change css theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import { Dashboard } from './Dashboard';
 function App() {
   return (
    <>
-      <h1><strong>Brutto REACAT che diventerà BELLIXXIMO!!</strong></h1>   
-      <div className="columns-3 p-8 flex justify-evenly">
+      <div className="columns-3 p-8 flex flex-wrap justify-evenly gap-x-2 bg-mint-600">
       <Dashboard/>
       </div>
    </>
